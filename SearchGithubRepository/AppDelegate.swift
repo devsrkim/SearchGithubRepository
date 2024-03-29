c7191486e6d6de7bd4fc4a6df3f11fe50881fb12
srkim <devsrkim@gmail.com>
2024-03-30 08:26:55 +0900

    프로젝트 생성

---
+++ SearchGithubRepository/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SearchGithubRepository
+//
+//  Created by srkim on 2024/03/30.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SearchGithubRepository/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchGithubRepository
+//
+//  Created by srkim on 2024/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
